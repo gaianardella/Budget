c29db8a1229f03f41cf084082220fbfe03304fae
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-12-22 16:06:56 +0100

    Convert expenses into positive values

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,9 @@ if __name__ == '__main__':
   file_path="monefy-2023-12-22_03-09-21.csv"
   data = pd.read_csv(file_path, delimiter=",")
   column_headers = list(data.columns.values)
+  # Keep only expenses
   data = data[~data["category"].isin(["Salary", "Deposits"])]
+
+  # Convert negative expenses into positive values
+  data = data["amount"].abs()
   st.bar_chart(data=data, x="category", y="amount") #, color=None, width=0, height=0, use_container_width=True)
