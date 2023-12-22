16d0d1eb3dc349fb8822d6c15c4d15e64b7c55a7
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-12-22 16:33:17 +0100

    Clean code

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,14 @@ import pandas as pd
 st.set_page_config(page_title="Budget App", page_icon=":moneybag:", layout="wide")
 
 if __name__ == '__main__':
+  # Read file and convert into DataFrame
   file_path="monefy-2023-12-22_03-09-21.csv"
   data = pd.read_csv(file_path, delimiter=",")
-  column_headers = list(data.columns.values)
-  st.write(column_headers)
+
+  # Print column names
+  # column_headers = list(data.columns.values)
+  # st.write(column_headers)
+  
   # Keep only expenses
   data = data[~data["category"].isin(["Salary", "Deposits"])]
   # Convert negative expenses into positive values
