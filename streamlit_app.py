5a7be69d2b987379cfe820ea7264b956945bad16
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-12-22 15:50:42 +0100

    Change page configuration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="A Cloud Closet", page_icon=":dress:", layout="wide")
+st.set_page_config(page_title="Budget App", page_icon=":moneybag:", layout="wide")
 
 if __name__ == '__main__':
   file_path="monefy-2023-12-22_03-09-21.csv"
   # data = pd.read_csv(file_path, delimiter=",")
-  data = pd.DataFrame(data)
+  data = pd.DataFrame(file_path)
   st.write(data)
   # data=st.dataframe(data)
   st.bar_chart(data)
