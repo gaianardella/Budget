243100d61d50d3348bfdbb6121999d49685dcb11
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-12-22 15:47:09 +0100

    Fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
   file_path="monefy-2023-12-22_03-09-21.csv"
   data = pd.read_csv(file_path, delimiter=",")
   st.write(data)
-  # st.dataframe(data)
+  data=st.dataframe(data)
   st.bar_chart(data)
