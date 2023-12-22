d66626e6d41427887bec791a957672144b973163
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-12-22 15:59:47 +0100

    Check dataframe columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,6 @@ st.set_page_config(page_title="Budget App", page_icon=":moneybag:", layout="wide
 if __name__ == '__main__':
   file_path="monefy-2023-12-22_03-09-21.csv"
   data = pd.read_csv(file_path, delimiter=",")
+  column_headers = list(data.columns.values)
+  exit()
   st.bar_chart(data=None, x=category, y=amount) #, color=None, width=0, height=0, use_container_width=True)
