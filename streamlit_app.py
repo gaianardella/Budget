06901e56d9bc388ae3af7fdd6d1e37b092aecc67
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-12-22 15:49:09 +0100

    Create dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ st.set_page_config(page_title="A Cloud Closet", page_icon=":dress:", layout="wid
 
 if __name__ == '__main__':
   file_path="monefy-2023-12-22_03-09-21.csv"
-  data = pd.read_csv(file_path, delimiter=",")
+  # data = pd.read_csv(file_path, delimiter=",")
+  data = pd.DataFrame(data)
   st.write(data)
-  data=st.dataframe(data)
+  # data=st.dataframe(data)
   st.bar_chart(data)
