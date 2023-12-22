baa5b4aeee03a19066e55a8546a99d4e53ef4acf
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-12-22 15:56:15 +0100

    Add bar chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,4 @@ st.set_page_config(page_title="Budget App", page_icon=":moneybag:", layout="wide
 if __name__ == '__main__':
   file_path="monefy-2023-12-22_03-09-21.csv"
   data = pd.read_csv(file_path, delimiter=",")
-  # data=st.dataframe(data)
-  st.write(data)
-  st.bar_chart(data)
+  st.bar_chart(data=None, x=data['category'], y=data['amount']) #, color=None, width=0, height=0, use_container_width=True)
